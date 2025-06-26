c15873334fa299403d72b1b123d85e83abe7d112
sudhanshuarya12345 <chachahamarevidhhayak147@gmail.com>
2025-06-26 12:12:29 +0530

    ninth commit

--- src/screens/Project.jsx
+++
@@ -1,8 +0,0 @@
-import hljs from 'highlight.js';
-import 'highlight.js/styles/nord.css';
-
-useEffect(() => {
-  document.querySelectorAll('pre code').forEach((block) => {
-    hljs.highlightElement(block);
-  });
-}, [messages]); 
