2076dc1403d17d3ca01d91982e1cc7298f124af7
sudhanshuarya12345 <chachahamarevidhhayak147@gmail.com>
2025-07-02 20:42:52 +0530

    14thCommit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,23 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server:{
+//     headers:{
+//       "Cross-Origin-Embedder-Policy": "credentialless",
+//       "Cross-Origin-Opener-Policy": "same-origin"
+//     },
+//     proxy:{
+//        '/cdn': {
+//         target: 'http://unpkg.com',
+//         changeOrigin: true,
+//         rewrite: (path) => path.replace(/^\/cdn/, '')
+//        }
+//     }
+//   }
+// })
+
 export default defineConfig({
   plugins: [react()],
   server:{
@@ -9,12 +26,5 @@ export default defineConfig({
       "Cross-Origin-Embedder-Policy": "credentialless",
       "Cross-Origin-Opener-Policy": "same-origin"
     },
-    proxy:{
-       '/cdn': {
-        target: 'http://unpkg.com',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/cdn/, '')
-       }
-    }
   }
 })
